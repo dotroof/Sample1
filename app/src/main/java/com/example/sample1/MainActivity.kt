437e2964168fa95c63dee1fbbe24c0394214090f
dotroof <dotroof@gmail.com>
2022-03-27 06:46:39 +0900

    DataBinding+LiveDataの実装

--- app/src/main/java/com/example/sample1/MainActivity.kt
+++ app/src/main/java/com/example/sample1/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.sample1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
+import com.example.sample1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+
+        binding.editText.addTextChangedListener { text ->
+            viewModel.updateButton(text.isNullOrBlank())
+        }
+        binding.button.setOnClickListener {
+            val text = binding.editText.text.toString()
+            viewModel.submitText(text)
+        }
     }
 }
